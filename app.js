642d6f56c71e533d3feb4908784d4129172afa69
GiovanniCarboni <giovanni.carboni.1997@gmail.com>
2023-03-14 15:57:06 +0200

    Added server.js

--- app.js
+++ app.js
@@ -6,10 +6,8 @@ const userRouter = require("./routes/userRoutes");
 const app = express();
 
 app.use(morgan("dev"));
+app.use(express.json());
 
 app.use("/api/v1/users", userRouter);
 
-const port = 3000;
-app.listen(port, () => {
-  console.log(`App running on port ${port}`);
-});
+module.exports = app;
