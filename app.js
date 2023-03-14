2ba33aae7fe2a71704e2b5fc17ed643ae65dd374
GiovanniCarboni <giovanni.carboni.1997@gmail.com>
2023-03-14 16:08:46 +0200

    Added middleware for static files

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const path = require("path");
+
 const express = require("express");
 const morgan = require("morgan");
 
@@ -7,6 +9,8 @@ const app = express();
 
 app.use(morgan("dev"));
 app.use(express.json());
+
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/api/v1/users", userRouter);
 
