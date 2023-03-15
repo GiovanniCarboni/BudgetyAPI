500cd3a96ae9e6035318eaa89643fbee2ee1ae59
GiovanniCarboni <giovanni.carboni.1997@gmail.com>
2023-03-15 18:54:01 +0200

    Added user model

---
+++ models/userModel.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+  },
+  email: {
+    type: String,
+    required: [true, "A user must have an email"],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, "Please provide a valid email"],
+  },
+  password: {
+    type: String,
+    required: [true, "Provide a password"],
+    minLength: 8,
+    select: false,
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, "Please confirm your password"],
+    validate: {
+      validator: function (el) {
+        return this.password === el;
+      },
+      message: "Passwords do not match",
+    },
+  },
+  passwordChangedAt: Date,
+  photo: {
+    type: String,
+    default: "default.jpg",
+  },
+  role: {
+    type: String,
+    enum: ["user", "admin"],
+    default: "user",
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
