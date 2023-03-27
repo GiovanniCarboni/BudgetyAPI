be6b25e8166832e4b571413ae5ba6dffce4fef78
GiovanniCarboni <giovanni.carboni.1997@gmail.com>
2023-03-27 14:56:13 +0300

    Added catch async

--- controllers/expenseController.js
+++ controllers/expenseController.js
@@ -1,101 +1,76 @@
 const Expense = require("../models/expenseModel");
+const catchAsync = require("../utils/catchAsync");
+const AppError = require("../utils/AppError");
 
-exports.getAllExpenses = async (req, res) => {
-  try {
-    const queryObj = { ...req.query };
+exports.getAllExpenses = catchAsync(async (req, res) => {
+  const queryObj = { ...req.query };
 
-    if (queryObj.tags)
-      queryObj.tags = { $in: queryObj.tags.replace(/_/, " ").split("-") };
+  if (queryObj.tags)
+    queryObj.tags = { $in: queryObj.tags.replace(/_/, " ").split("-") };
 
-    const excludedFields = ["page", "sort", "limit", "fields"];
-    excludedFields.forEach((field) => delete queryObj[field]);
+  const excludedFields = ["page", "sort", "limit", "fields"];
+  excludedFields.forEach((field) => delete queryObj[field]);
 
-    const expenses = await Expense.find(queryObj);
+  const expenses = await Expense.find(queryObj);
 
-    res.status(200).json({
-      status: "success",
-      results: expenses.length,
-      data: {
-        expenses,
-      },
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: "fail",
-      message: err,
-    });
-  }
-};
+  res.status(200).json({
+    status: "success",
+    results: expenses.length,
+    data: {
+      expenses,
+    },
+  });
+});
 
-exports.getExpense = async (req, res) => {
-  try {
-    const expense = await Expense.findById(req.params.id);
+exports.getExpense = catchAsync(async (req, res, next) => {
+  const expense = await Expense.findById(req.params.id);
 
-    res.status(200).json({
-      status: "success",
-      data: {
-        expense,
-      },
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: "fail",
-      message: err,
-    });
-  }
-};
+  if (!expense) return next(new AppError("No tour found with that id", 404));
 
-exports.createExpense = async (req, res) => {
-  try {
-    const newExpense = await Expense.create(req.body);
+  res.status(200).json({
+    status: "success",
+    data: {
+      expense,
+    },
+  });
+});
 
-    res.status(200).json({
-      status: "success",
-      data: {
-        expense: newExpense,
-      },
-    });
-  } catch (err) {
-    res.status(400).json({
-      status: "fail",
-      message: err,
-    });
-  }
-};
-exports.updateExpense = async (req, res) => {
-  try {
-    // new: true means that expense is the doc after update
-    const expense = await Expense.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true,
-    });
+exports.createExpense = catchAsync(async (req, res) => {
+  const newExpense = await Expense.create(req.body);
 
-    res.status(200).json({
-      status: "success",
-      data: {
-        expense,
-      },
-    });
-  } catch (err) {
-    res.status(400).json({
-      status: "fail",
-      message: err,
-    });
-  }
-};
-exports.deleteExpense = async (req, res) => {
-  try {
-    // new: true means that expense is the doc after update
-    const expense = await Expense.findByIdAndDelete(req.params.id);
+  res.status(200).json({
+    status: "success",
+    data: {
+      expense: newExpense,
+    },
+  });
+});
 
-    res.status(200).json({
-      status: "success",
-      data: null,
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: "fail",
-      message: err,
-    });
-  }
-};
+exports.updateExpense = catchAsync(async (req, res) => {
+  // new: true means that expense is the doc after update
+  const expense = await Expense.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
+  if (!expense) return next(new AppError("No tour found with that id", 404));
+
+  res.status(200).json({
+    status: "success",
+    data: {
+      expense,
+    },
+  });
+});
+
+exports.deleteExpense = catchAsync(async (req, res) => {
+  // new: true means that expense is the doc after update
+  const expense = await Expense.findByIdAndDelete(req.params.id);
+
+  if (!expense) return next(new AppError("No tour found with that id", 404));
+
+  res.status(200).json({
+    status: "success",
+    data: null,
+  });
+});
